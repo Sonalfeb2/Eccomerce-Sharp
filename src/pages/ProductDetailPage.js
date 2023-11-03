9eac42e4ff4c81bc5261b39fa65513ad3f9ff7ce
Sonal Agrawal <sonalfeb7@gmail.com>
2023-11-03 16:35:49 +0530

    Worked on View More Functionality in Store Page

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,39 +1,54 @@
 import { useParams } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
-import { Col, Image ,Card,Row} from "react-bootstrap";
+import { Col, Image, Card, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 const ProductDetailPage = () => {
   const params = useParams();
   console.log(params);
   const [productDetail, setProductDetail] = useState(null);
-  useEffect(() => {
-    fetch(`https://dummyjson.com/products/${params.id}`)
-      .then(res => res.json())
-      .then((data)=>setProductDetail(data));
-  }, [params.id]);
-  console.log(productDetail)
+  useEffect(
+    () => {
+      fetch(`https://dummyjson.com/products/${params.id}`)
+        .then(res => res.json())
+        .then(data => setProductDetail(data));
+    },
+    [params.id]
+  );
+  console.log(productDetail);
   return (
-    <Row>
-    <Col xs={6} className="bg-dark">
-      <Carousel>
-        {productDetail!==null&&productDetail.images.map((image)=><Carousel.Item>
-          <Image src={image} className="w-100"/>
-        </Carousel.Item>)}
-      </Carousel>
-    </Col>
-     <Col xs={6}>
-     <Card>
-      <Card.Body>
-        <Card.Title>{productDetail.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{productDetail.category} Price: ${productDetail.price}</Card.Subtitle>
-        <Card.Subtitle className="mb-2 text-muted">Brand: {productDetail.brand}</Card.Subtitle>
-        <Card.Text>
-         Summary:  {productDetail.description}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-     </Col>
-    </Row>
+    <div>
+      {productDetail !== null &&
+        <Row>
+          <Col xs={6} className="bg-dark">
+            <Carousel>
+              {productDetail.images.map(image =>
+                <Carousel.Item>
+                  <Image src={image} className="w-100" />
+                </Carousel.Item>
+              )}
+            </Carousel>
+          </Col>
+          <Col xs={6}>
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  {productDetail.title}
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {productDetail.category} Price: ${productDetail.price}
+                </Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Brand: {productDetail.brand}
+                </Card.Subtitle>
+                <Card.Text>
+                  Summary: {productDetail.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>}
+      {productDetail == null && <p>Loading...</p>}
+    </div>
   );
 };
 export default ProductDetailPage;
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -3,6 +3,7 @@ import GenricsCard from "../layout/Card";
 import { Button } from "react-bootstrap";
 const Store = () => {
   const [product, setProduct] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then(data => data.json())
@@ -10,14 +11,13 @@ const Store = () => {
         setProduct(data.products);
       });
   }, []);
-
   return (
     <div>
       <h1 className="text-center">PRODUCTS</h1>
       <div className="row">
         {product.map((item, index) => {
           return (
-            index < 10 &&
+            index < pageCount * 10 &&
             <div className="col-lg-6" key={item.id}>
               <GenricsCard
                 title={item.title}
@@ -30,7 +30,13 @@ const Store = () => {
         })}
       </div>
       <div className="d-flex justify-content-center">
-        <Button variant="secondary">See the Cart </Button>
+        {(pageCount + 1) * 10 <= product.length && pageCount*10 < product.length &&
+          <Button
+            variant="secondary"
+            onClick={()=>{setPageCount(pageCount+1)}}
+          >
+            View More{" "}
+          </Button>}
       </div>
     </div>
   );
