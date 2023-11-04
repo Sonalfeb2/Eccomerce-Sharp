98147f08b23ef4a456d1494727035224031552a3
Sonal Agrawal <sonalfeb7@gmail.com>
2023-11-04 17:55:21 +0530

    Connected Firebase DB and Add a Toast UI For Auth Msg , Added Loading Functionality In the Phase of Request

--- src/Auth/AuthForm.js
+++ src/Auth/AuthForm.js
@@ -1,43 +1,91 @@
-import { useState} from 'react';
-
-import classes from './AuthForm.module.css';
+import { useState, useRef } from "react";
+import { Toast, ToastContainer } from "react-bootstrap";
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-
+  const [isloading, setIsLoading] = useState(false);
+  const [showMsg, setShowMsg] = useState(false);
+  const email = useRef();
+  const password = useRef();
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
+    setIsLogin(prevState => !prevState);
   };
-
+  const AuthHandler = async e => {
+    e.preventDefault();
+    setIsLoading(true);
+    const category = isLogin ? "Login" : "Signup";
+    const response = await fetch(
+      `https://mypprojectauth-default-rtdb.firebaseio.com/${category}.json`,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email.current.value,
+          password: password.current.value
+        })
+      }
+    );
+    const data = await response.json();
+    if (data.name) {
+      setIsLoading(false);
+      setShowMsg(true);
+      setTimeout(() => setShowMsg(false), 3000);
+    }
+  };
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <h1>
+        {isLogin ? "Login" : "Sign Up"}
+      </h1>
+      <form onSubmit={AuthForm}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={email} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            required
-          />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" ref={password} required />
         </div>
         <div className={classes.actions}>
-        <button type='button' className={classes.button} >{isLogin?'SignIn' : 'SignUp'}</button>
+          {!isloading
+            ? <button
+                type="submit"
+                className={classes.button}
+                onClick={AuthHandler}
+              >
+                {isLogin ? "SignIn" : "SignUp"}
+              </button>
+            : <p>Sending Request...</p>}
         </div>
+
         <div className={classes.actions}>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
+      {showMsg &&
+        <ToastContainer
+          className="p-3"
+          position="top-center"
+          style={{ zIndex: 1 }}
+        >
+          <Toast>
+            <Toast.Header closeButton={false}>
+              <strong className="me-auto">Message</strong>
+            </Toast.Header>
+            <Toast.Body>
+              {isLogin ? "Login SuccessFully" : "SignUp SuccessFully"}
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>}
     </section>
   );
 };
